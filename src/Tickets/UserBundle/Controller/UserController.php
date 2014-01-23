19a02651d0855b76bddbb4fdfade1e9d8e6c6531
sensukho <sensukho@gmail.com>
2014-01-23 00:51:22 -0600

    Ver 1.0 => Update::View tickets

--- src/Tickets/UserBundle/Controller/UserController.php
+++ src/Tickets/UserBundle/Controller/UserController.php
@@ -8,21 +8,26 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TicketsUserBundle:Login:index.html.twig', array());
+        return $this->render('TicketsUserBundle:User:index.html.twig', array());
     }
 
     public function newticketAction()
     {
-        return $this->render('TicketsUserBundle:Login:newticket.html.twig', array());
+        return $this->render('TicketsUserBundle:User:newticket.html.twig', array());
     }
 
     public function ticketsAction()
     {
-        return $this->render('TicketsUserBundle:Login:tickets.html.twig', array());
+        return $this->render('TicketsUserBundle:User:tickets.html.twig', array());
     }
 
     public function seeticketsAction()
     {
-        return $this->render('TicketsUserBundle:Login:seetickets.html.twig', array());
+        return $this->render('TicketsUserBundle:User:seetickets.html.twig', array());
+    }
+
+    public function seeticketAction($id)
+    {
+        return $this->render('TicketsUserBundle:User:seeticket.html.twig', array("id" => $id));
     }
 }
